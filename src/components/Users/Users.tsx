83493bec0a77e691e24fa0a9f42f6b45ec542e67
maximkuznecovv <maximkuznecovv@gmail.com>
2021-04-15 00:02:32 +0300

    55 - pagination, постраничный вывод пользователей

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
-import {UsersPropsType} from './UsersContainer';
-import {UsersInitialStatePropsType} from '../../redux/users-reducer';
-import axios from 'axios';
-import userPhoto from '../../assets/images/user.png'
+import React from "react";
+import {UsersPropsType} from "./UsersContainer";
+import axios from "axios";
+import userPhoto from "../../assets/images/user.png"
+import styles from "./UsersContainer.module.css"
+
+type ResponseItemType = {
+    "name": string
+    "id": number
+    "uniqueUrlName": null | string
+    "photos": {
+        "small": null | string
+        "large": null | string
+    },
+    "status": null | string
+    "followed": boolean
+}
+type ServerData = {
+    "items": ResponseItemType[]
+    "totalCount": number
+    "error": null | string
+}
+type ResponseType = {
+    data: ServerData
+}
 
 export class Users extends React.Component<UsersPropsType> {
 
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            this.props.setUsers(response.data.items)
-        })
+        axios.get<ServerData>(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then((response: ResponseType) => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged = (page: number) => {
+        this.props.setCurrentPage(page)
+
+        axios.get<ServerData>(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
+            .then((response: ResponseType) => {
+                this.props.setUsers(response.data.items)
+            })
     }
 
     render() {
@@ -18,7 +49,7 @@ export class Users extends React.Component<UsersPropsType> {
                 <div key={u.id}>
                     <div>
                         <img src={u.photos.small !== null ? u.photos.small : userPhoto}
-                             style={{width: '100px', height: '100px'}} alt={'Avatar'}/>
+                             className={styles.userPhoto} alt={"Avatar"}/>
                     </div>
                     <div>
                         {u.followed
@@ -34,17 +65,31 @@ export class Users extends React.Component<UsersPropsType> {
                         <div>{u.status}</div>
                     </div>
                     <div>
-                        <div>{'u.location.country'}</div>
-                        <div>{'u.location.city'}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </div>
                 </div>
             )
         })
 
+        let pagesCount = Math.ceil(this.props.totalCount / this.props.pageSize)
+        const pages = []
+
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+
         return <div>
+            {pages.map(page => {
+                return <span
+                    className={this.props.currentPage === page ? styles.selectedPage : ""}
+                    onClick={() => {
+                        this.onPageChanged(page)
+                    }}
+                >{page}</span>
+            })}
             {UsersEl}
         </div>
     }
 }
-
 
--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -1,40 +1,28 @@
-export const FOLLOW = 'FOLLOW'
-export const UNFOLLOW = 'UNFOLLOW'
-export const SET_USERS = 'SET_USERS'
+export const FOLLOW = "FOLLOW"
+export const UNFOLLOW = "UNFOLLOW"
+export const SET_USERS = "SET_USERS"
+export const SET_CURRENT_PAGE = "SET_CURRENT_PAGE"
+export const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT"
 
 
- export type UsersInitialStatePropsType = {
-"name": string
-"id": number
-"uniqueUrlName": string | null
-"photos": {
-    "small": string | null
-    "large": string | null
-},
-"status": string | null
-"followed": boolean
+export type UsersInitialStatePropsType = {
+    "name": string
+    "id": number
+    "uniqueUrlName": string | null
+    "photos": {
+        "small": string | null
+        "large": string | null
+    },
+    "status": string | null
+    "followed": boolean
 }
 
 
 const initialState = {
-        users: [] as UsersInitialStatePropsType[]
-}
-
-export type UsersInitialStateType = typeof initialState
-
-
-type ActionsType = ReturnType<typeof followAC>
-    | ReturnType<typeof unfollowAC>
-    | ReturnType<typeof seUsersAC>
-
-export const followAC = (userID: number) => {
-    return {type: FOLLOW, userID} as const
-}
-export const unfollowAC = (userID: number) => {
-    return {type: UNFOLLOW, userID} as const
-}
-export const seUsersAC = (users: UsersInitialStatePropsType[]) => {
-    return {type: SET_USERS, users} as const
+    users: [] as UsersInitialStatePropsType[],
+    pageSize: 10,
+    totalCount: 0,
+    currentPage: 1
 }
 
 export const usersReducer = (state: UsersInitialStateType = initialState, action: ActionsType): UsersInitialStateType => {
@@ -60,10 +48,39 @@ export const usersReducer = (state: UsersInitialStateType = initialState, action
                     return u
                 })
             }
-
         case SET_USERS:
-            return {...state, users: [ ...state.users, ...action.users]}
+            return {...state, users: [...action.users]}
+        case SET_CURRENT_PAGE:
+            return {...state, currentPage: action.currentPage}
+        case SET_TOTAL_USERS_COUNT:
+            return {...state, totalCount: action.totalCount}
         default:
             return state
     }
+}
+
+export type UsersInitialStateType = typeof initialState
+
+
+type ActionsType = ReturnType<typeof followAC>
+    | ReturnType<typeof unfollowAC>
+    | ReturnType<typeof setUsersAC>
+    | ReturnType<typeof setCurrentPageAC>
+    | ReturnType<typeof setUsersTotalCountAC>
+
+export const followAC = (userID: number) => {
+    return {type: FOLLOW, userID} as const
+}
+export const unfollowAC = (userID: number) => {
+    return {type: UNFOLLOW, userID} as const
+}
+export const setUsersAC = (users: UsersInitialStatePropsType[]) => {
+    return {type: SET_USERS, users} as const
+}
+export const setCurrentPageAC = (currentPage: number) => {
+    return {type: SET_CURRENT_PAGE, currentPage} as const
+}
+
+export const setUsersTotalCountAC = (totalCount: number) => {
+    return {type: SET_TOTAL_USERS_COUNT, totalCount} as const
 }
